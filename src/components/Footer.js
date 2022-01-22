6839a6a8e31358e7fc7549781361df954b55b6f3
martinsideas <acadecoder@gmail.com>
2022-01-22 18:09:01 -0500

    Main & Footer Styles finished.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,15 @@ export default function Footer(){
     return (
 <>
         <footer className="footer">
-            <img className="tw-ic" src={tw} alt="Twitter Profile" />
-            <img className="ig-ic" src={ig} alt="Instagram Profile" />
-            <img className="gh-ic" src={gh} alt="Github Profile" />
+            <a href="https://twitter.com/martinsideas" target="_blank" rel="noopener noreferrer">
+                <img className="tw-ic" src={tw} alt="Twitter Profile" />
+            </a>
+            <a href="http://instagram.com/martinsideas" target="_blank" rel="noopener noreferrer">
+                <img className="ig-ic" src={ig} alt="Instagram Profile" />
+            </a>
+            <a href="http://github.com/martinsideas" target="_blank" rel="noopener noreferrer">
+                <img className="gh-ic" src={gh} alt="Github Profile" />
+            </a>
         </footer>
 </>
     )
